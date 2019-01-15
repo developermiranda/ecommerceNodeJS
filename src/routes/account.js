e554da2e420bc6ad11641295d51871455c4a5e8b
Daniel Miranda <developermiranda@gmail.com>
2019-01-15 12:40:08 -0200

    Criando Novo Usuário - Definindo Rotas e Model

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const Customer = require('./../schemas/customer')
+
 router.get('/', (req, res) => {
   return res.render('account/index', {
     title: 'Account',
@@ -9,10 +11,17 @@ router.get('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
+  const customer = new Customer()
+  
   return res.render('account/new', {
     title: "Account - Register",
+    customer: customer,
     layout: 'layouts/main'
   })
+})
+
+router.post('/', (req, res) => {
+
 })
 
 module.exports = router
