45965626a299703d7084f15a0420872eebaad190
Daniel Miranda <developermiranda@gmail.com>
2019-01-15 11:28:26 -0200

    Criando Página de Registro e de Login

--- src/routes/account.js
+++ src/routes/account.js
@@ -2,12 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  return res.render('main/index', {
-    title: 'Teste Ecommerce',
-    layout: 'layouts/base'
-
-  })
+  return res.render('account/index', {
+    title: 'Account',
+    layout: 'layouts/main'
+  });
 })
-
-
 module.exports = router
